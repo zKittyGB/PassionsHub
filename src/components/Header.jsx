62fa7223e7d5f40e59104d1bf1980bdd2e3af822
zKittyGB <a-hebert@hotmail.fr>
2025-11-26 11:36:19 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import '../css/Header.css'
+function button() {}
+
+function Header() {
+
+	return (
+		<>
+			<div className='header'>
+				<h1>Communauté de Loisirs & Passions</h1>
+				<div className='actions'>
+					<button className='btn' onClick={button}>Connexion</button>
+					<button className='btn filled' onClick={button}>Inscription</button>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Header
