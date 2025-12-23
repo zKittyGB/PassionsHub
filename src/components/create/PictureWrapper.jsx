cf6f16369a37fe961bbd90ee6d9a917ea828f732
zKittyGB <a-hebert@hotmail.fr>
2025-12-23 17:58:34 +0100

    update some comments

--- src/components/create/PictureWrapper.jsx
+++ src/components/create/PictureWrapper.jsx
@@ -1,28 +1,33 @@
+// Component to render a single picture upload input with preview
 function PictureWrapper({ ID, pictures, setPictures, setErrors }) {
 	const currentPicture = pictures[ID];
 
 	return (
 		<div className="picture-wrapper">
+			{/* Hidden file input for image upload */}
 			<input
 				type="file"
 				id={`picture_${ID}`}
 				name={`picture_${ID}`}
 				accept="image/*"
 				onChange={(e) => {
+					// Update the selected picture in the state
 					setPictures(prev => {
 						const copy = [...prev];
 						copy[ID] = e.target.files[0];
 						return copy.filter(Boolean);
 					});
+					// Clear picture-related errors
 					setErrors(prev => ({ ...prev, pictures: [] }));
 				}}
 			/>
 
+			{/* Label used as clickable area for file input */}
 			<label htmlFor={`picture_${ID}`}>
-				{/* Affiche l'icône si aucune image n'est sélectionnée */}
+				{/* Display camera icon if no image is selected */}
 				{!currentPicture && <i className="fa-solid fa-camera"></i>}
 
-				{/* Affiche la miniature */}
+				{/* Display preview of selected image */}
 				{currentPicture && (
 					<img
 						src={URL.createObjectURL(currentPicture)}
